baf8ac86beaa02c9894084eac636975ff717d97d
Reda Ali <reda5201028@gmail.com>
2022-06-22 23:40:07 +0200

    Improve books page

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,12 +1,18 @@
 <x-layout>
     <div class="w-4/5">
-        <div class="p-7 grid md:grid-cols-3 xs:grid-cols-1 gap-4">
-            @foreach ($books as $book)
-                <x-book-cards :book="$book" />
-            @endforeach
-        </div>
-        <div class=" flex justify-center">
-            {{ $books->links() }}
-        </div>
+        @if (count($books))
+            <div class="p-7 grid md:grid-cols-3 xs:grid-cols-1 gap-4">
+                @foreach ($books as $book)
+                    <x-book-cards :book="$book" />
+                @endforeach
+            </div>
+            <div class=" flex justify-center">
+                {{ $books->links() }}
+            </div>
+        @else
+            <div class="p-7 font-semibold text-3xl">
+                There is no books yet.
+            </div>
+        @endif
     </div>
 </x-layout>
